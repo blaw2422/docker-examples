6b390809772e8677ce5f32a247ac62fcc2513eca
Brandon Law <blaw@invivolink.com>
2018-01-15 19:44:13 -0600

    4- multi local

--- 4-multi-local-service/js-app/app.js
+++ 4-multi-local-service/js-app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const config = require('./config');
+const http = require('http');
 
 const express = require('express')
 const app = express();
@@ -30,4 +31,23 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/redis', (req, res) => redisClient().set(req.query.key, req.query.value, redisSetCb(req, res)));
 app.get('/redis', (req, res) => redisClient().get(req.query.key, redisGetCb(req, res)));
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.get('/python', (req, res) => {
+const options = {
+    host: 'python-service',
+    port: 5000,
+    path: '/',
+    method: 'GET'
+};
+
+http.request(options, function(pythonRes) {
+    console.log('STATUS: ' + pythonRes.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(pythonRes.headers));
+    pythonRes.setEncoding('utf8');
+    pythonRes.on('data', function (chunk) {
+          console.log('BODY: ' + chunk);
+          res.send(' from python-service' + chunk);
+        });
+  }).end();
+
+});
+app.listen(8000, () => console.log('Example app listening on port 8000! ooooooohhh   weeee'))
